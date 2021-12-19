bd42b2ce0d6b038fd28483a62c8dd877cf01916a
Katie Riedinger <96099858+katiejriedinger@users.noreply.github.com>
2021-12-19 15:52:34 -0600

    Add files via upload

---
+++ Mad Libs.py
@@ -0,0 +1,12 @@
+print("Welcome to Mad Libs! Choose your words wisely.")
+print()
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Coding is so {adj}! It makes me so happy because I love to {verb1}. \
+Keep being awesome and {verb2} like you are {famous_person}!"
+
+print(madlib)
